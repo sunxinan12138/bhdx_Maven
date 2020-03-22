958ce94e4fe31b10c44c5806284c0ba44f78fa43
zjm12138 <2450961739@qq.com>
2020-03-22 16:04:37 +0800

    管理员审核界面

---
+++ src/main/java/com/bhdx/tools/StringSplittingTool.java
@@ -0,0 +1,28 @@
+package com.bhdx.tools;
+
+import com.bhdx.models.Admin;
+
+public class StringSplittingTool {
+    //2017 1605 01
+    //电子 17级 1班
+    public String  GetSubject(String ID){
+         String Subject = ID.substring(4,8);
+         switch (Subject){
+             case "1601" : Subject = "测控";
+             break;
+             case "1602" : Subject = "电气";
+             break;
+             case "1603" : Subject = "信息";
+             break;
+             case "1604" : Subject = "通信";
+             break;
+             case "1605" : Subject = "电子";
+             break;
+             case "1606" : Subject = "自动化";
+             break;
+             case "1607" : Subject = "通合作";
+         }
+         String New_ID=Subject+ID.substring(2,4)+"级"+ID.substring(9,10)+"班";
+         return New_ID;
+     }
+}
